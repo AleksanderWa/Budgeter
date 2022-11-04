f69225b94d574cab0f3615f600cfcf559d58fca0
awalkowski <awalkowski@bsbox.pl>
2022-11-04 18:12:13 +0100

    user creation endpoint + simple test

--- app/urls.py
+++ app/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from budget.views import UserCreate
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("account/register", UserCreate.as_view(), name="register"),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- budget/tests.py
+++ budget/tests.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+
+class UserCreateTest(TestCase):
+    def test_can_create(self):
+        url = reverse("register")
+        response = self.client.post(url, {"username": "Batman", "password": "abc321"})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(username="Batman").exists())
